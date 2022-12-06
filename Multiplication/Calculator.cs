12c24f82e89f0c2a6bf93ca64ee859246e847fcb
rahulgundawar98 <rahulgundawar98@gmail.com>
2022-12-06 16:58:47 +0530

    [Rahul] Adding multiplication project

---
+++ Multiplication/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication
+{
+    class Multiplication
+    {
+        public void Multiply(int firstnumber ,int secondnumber)
+        {
+            int result = firstnumber * secondnumber;
+            Console.WriteLine(  "Multiply is :"+result);
+        }
+    }
+}
---
+++ Multiplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Intrinsics.Arm;
+
+namespace Multiplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Multiplication mul = new Multiplication();
+            Console.WriteLine("please enter first number");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter second number");
+            int second = Convert.ToInt32(Console.ReadLine());
+            mul.Multiply(first, second);
+            Console.ReadLine();
+        }
+    }
+
+
+}
